29946b4c0b7b7a12f74e97a8cde188386235e037
xlgwr <xlgwr@163.com>
2019-07-21 19:42:37 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + @"Template\Template311.xls";
+            string filepath2 = AppDomain.CurrentDomain.BaseDirectory + @"Template\Template311x.xlsx";
+
+            var pdfhelp = new ToPdfHelper("xls");
+
+            pdfhelp.XlsWpsToPdf(filepath, "Template311.xls");
+
+            Console.ReadKey();
+        }
+    }
+}
